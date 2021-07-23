eab9f7ecddc864341369a69e54ea504f6746bfc3
김은영 <solaz0824@gmail.com>
2021-07-23 09:28:44 +0200

    Basic jsGrid table

--- assets/js/jsGrid.js
+++ assets/js/jsGrid.js
@@ -1,100 +1,97 @@
-$.getJSON("", function (data) {
-  console.log(data);
-  $("#employeesList").jsGrid({
-    height: "85vh",
-    width: "100%",
+$("#employeesList").jsGrid({
+  height: "85vh",
+  width: "100%",
 
-    editing: false,
-    sorting: true,
-    paging: true,
-    autoload: true,
-    pageSize: 15,
-    pageButtonCount: 3,
-    deleteConfirm: "Do you confirm you want to delete employee?",
+  editing: false,
+  sorting: true,
+  paging: true,
+  autoload: true,
+  pageSize: 15,
+  pageButtonCount: 3,
+  deleteConfirm: "Do you confirm you want to delete employee?",
 
-    controller: {
-      loadData: function () {
-        return $.ajax({
-          type: "GET",
-          url: ``,
-        });
-      },
-      deleteItem: function (item) {
-        return $.ajax({
-          type: "DELETE",
-          url: ``,
-          data: item,
-        });
-      },
+  controller: {
+    loadData: function () {
+      return $.ajax({
+        type: "GET",
+        url: ``,
+      });
     },
-
-    fields: [
-      {
-        name: "name",
-        validate: "required",
-        title: "Name",
-        type: "text",
-        align: "",
-        width: 150,
-      },
-      {
-        name: "email",
-        validate: "required",
-        title: "Email",
-        type: "text",
-        align: "",
-        width: 200,
-      },
-      {
-        name: "age",
-        validate: function (value) {
-          return value > 0;
-        },
-        title: "Age",
-        type: "text",
-        align: "",
-        width: 50,
-      },
-      {
-        name: "streetAddress",
-        title: "Street No.",
-        type: "number",
-        align: "",
-        width: 100,
-      },
-      { name: "city", title: "City", type: "text", align: "", width: 100 },
-      { name: "state", title: "State", type: "text", align: "", width: 50 },
-      {
-        name: "postalCode",
-        title: "Postal Code",
-        type: "number",
-        align: "",
-        width: 100,
-      },
-      {
-        name: "phoneNumber",
-        title: "Phone Number",
-        type: "number",
-        align: "",
-        width: 200,
-      },
-      {
-        type: "control",
-        modeSwitchButton: false,
-        editButton: false,
-        headerTemplate: function () {
-          return $("<button>")
-            .attr("type", "button")
-            .attr("class", "jsgrid-button jsgrid-insert-button")
-            .on("click", function () {
-              //   createNewEmployee();
-            });
-        },
-        width: 100,
-      },
-    ],
-    rowClick: function (item) {
-      //   editEmployee(item);
+    deleteItem: function (item) {
+      return $.ajax({
+        type: "DELETE",
+        url: ``,
+        data: item,
+      });
     },
-  });
+  },
+
+  fields: [
+    {
+      name: "name",
+      validate: "required",
+      title: "Name",
+      type: "text",
+      align: "",
+      width: 150,
+    },
+    {
+      name: "email",
+      validate: "required",
+      title: "Email",
+      type: "text",
+      align: "",
+      width: 200,
+    },
+    {
+      name: "age",
+      validate: function (value) {
+        return value > 0;
+      },
+      title: "Age",
+      type: "text",
+      align: "",
+      width: 50,
+    },
+    {
+      name: "streetAddress",
+      title: "Street No.",
+      type: "number",
+      align: "",
+      width: 100,
+    },
+    { name: "city", title: "City", type: "text", align: "", width: 100 },
+    { name: "state", title: "State", type: "text", align: "", width: 50 },
+    {
+      name: "postalCode",
+      title: "Postal Code",
+      type: "number",
+      align: "",
+      width: 100,
+    },
+    {
+      name: "phoneNumber",
+      title: "Phone Number",
+      type: "number",
+      align: "",
+      width: 200,
+    },
+    {
+      type: "control",
+      modeSwitchButton: false,
+      editButton: false,
+      headerTemplate: function () {
+        return $("<button>")
+          .attr("type", "button")
+          .attr("class", "jsgrid-button jsgrid-insert-button")
+          .on("click", function () {
+            //   createNewEmployee();
+          });
+      },
+      width: 100,
+    },
+  ],
+  rowClick: function (item) {
+    //   editEmployee(item);
+  },
 });
--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set("display_errors", "On");
-echo NODE_MODULES;
+echo "Dashboard View(Employees)";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,7 +10,7 @@ echo NODE_MODULES;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>File System Explorer</title>
+    <title>Dashboard</title>
 
     <!-- Script del CDN de Jquery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
